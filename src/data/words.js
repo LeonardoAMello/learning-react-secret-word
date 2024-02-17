8c7b292199e9b4b2a631b9a3c6b4e4748471d8b6
Leonardo Mello <leonardodeabreumello@gmail.com>
2024-02-17 11:57:42 -0300

    Reset CSS and README.md made by Vite

---
+++ src/data/words.js
@@ -0,0 +1,13 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Freio", "Farol", "Cinto", "Vidro", "Buzina", "Retrovisor", "Combustível", "Chave", "Volante", "Suspensão", "Marcha", "Ignição", "Embreagem", "Radiador", "Óleo"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Melancia", "Uva", "Morango", "Abacaxi", "Kiwi", "Cereja", "Pêssego", "Melão", "Figo", "Goiaba", "Coco", "Ameixa", "Pitanga", "Tangerina", "Framboesa"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Orelha", "Dedos", "Joelho", "Cotovelo", "Cabelo", "Língua", "Nariz", "Cintura", "Costas", "Canela", "Pulso", "Tornozelo", "Dente", "Estômago", "Peito"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Memória", "Processador", "Placa-mãe", "Disco rígido", "SSD", "Fonte de alimentação", "Placa de vídeo", "Cooler", "USB", "HDMI", "Wi-Fi", "Bluetooth", "Webcam", "Microfone", "Alto-falante", "Energia"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Node.js", "HTML", "CSS", "Python", "Java", "C#", "API", "Biblioteca", "SQL", "Backend", "Frontend", "Git", "Terminal", "IDE", "Debugging", "Compilação"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Pão", "Queijo", "Tomate", "Cebola", "Alface", "Batata", "Cenoura", "Peixe", "Frango", "Iogurte", "Mel", "Abacate", "Azeite", "Chocolate", "Sorvete"],
+    animal: ["Elefante", "Tigre", "Girafa", "Leão", "Cachorro", "Gato", "Macaco", "Panda", "Coelho", "Pinguim", "Hipopótamo", "Rinoceronte", "Cobra", "Águia", "Tubarão", "Polvo", "Borboleta", "Leopardo", "Gorila", "Crocodilo"],
+    país: ["Brasil", "Estados Unidos", "Japão", "França", "Itália", "China", "Índia", "Rússia", "Canadá", "México", "Austrália", "Alemanha", "Reino Unido", "Espanha", "Argentina", "Chile", "Portugal", "Suíça", "África do Sul", "Coreia do Sul"],
+    esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Ciclismo", "Vôlei", "Golfe", "Handebol", "Atletismo", "Surfe", "Boxe", "Hóquei", "Esqui", "Canoagem", "Ginástica", "Rúgbi", "Mergulho", "Patinagem", "Curling", "Badminton"],
+    profissão: ["Médico", "Professor", "Engenheiro", "Artista", "Advogado", "Cientista", "Programador", "Jornalista", "Psicólogo", "Policial", "Bombeiro", "Piloto", "Chef", "Arquiteto", "Designer", "Enfermeiro", "Farmacêutico", "Astronauta", "Eletricista", "Empresário"],
+    música: ["Guitarra", "Bateria", "Violino", "Piano", "Flauta", "Baixo", "Saxofone", "Trompete", "Violoncelo", "Tambor", "Harpa", "Acordeão", "Cavaquinho", "Gaita de foles", "Maracas", "Pandeiro", "Triângulo", "Sinos", "Sintetizador", "Microfone"]
+};
