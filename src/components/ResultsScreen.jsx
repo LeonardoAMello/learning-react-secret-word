cc94a7fcd277a0ca02a6de07078621b270b7c6d2
Leonardo Mello <leonardodeabreumello@gmail.com>
2024-02-25 20:16:46 -0300

    End screen

--- src/components/ResultsScreen.jsx
+++ src/components/ResultsScreen.jsx
@@ -1,9 +1,11 @@
 import './ResultsScreen.css'
 
-const ResultsScreen = ({returnStart}) => {
+const ResultsScreen = ({returnStart, score}) => {
   return (
     <div>
       <h1>Secret Word</h1>
+      <h1>Fim de jogo!</h1>
+      <h2>A sua pontuação foi: <span>{score}</span></h2>
       <button onClick={returnStart}>Voltar para o início</button>
     </div>
   )
