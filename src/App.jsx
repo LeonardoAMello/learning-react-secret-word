6844ee38f78ea835e5da4cdfe45eff95b3c5309c
Leonardo Mello <leonardodeabreumello@gmail.com>
2024-02-25 19:52:08 -0300

    Main funcionalities of game

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,78 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+
+  const [score, setScore] = useState(0);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [retries, setRetries] = useState(5);
   
   const startGame = () => {
+    chooseWord();
     setGameStage(stages[1].name);
   }
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toUpperCase();
+
+    if(guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+      return;
+    }
+
+    if(letters.includes(normalizedLetter)) {
+      setGuessedLetters((actual) => [...actual, normalizedLetter]);
+    }
+    else {
+      setWrongLetters((actual) => [...actual, normalizedLetter]);
+      setRetries((actual) => actual - 1);
+    }
   }
+
+  useEffect(() => {
+    const uniqueLetters = letters.filter((item, i, ar) => ar.indexOf(item) === i);
+    console.log(guessedLetters, letters);
+    if(guessedLetters.length > 0 && guessedLetters.length == uniqueLetters.length) {
+      setScore((actual) => actual + 100);
+      chooseWord();
+    }
+
+    if(retries == 0) {
+      finishGame();
+    }
+  }, [guessedLetters, retries])
+  
+
+  const chooseWord = () => {
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * categories.length)]
+
+    const currentWords = words[category]
+    const word = currentWords[Math.floor(Math.random() * currentWords.length)]
+
+    const wordLetters = word.split("").map((l) => l.toUpperCase());
+
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  const finishGame = () => {
+    // Reseting all game session data
+    setPickedWord("");
+    setPickedCategory("");
+    setLetters([]);
+    setGuessedLetters([]);
+    setWrongLetters([]);
+    setRetries(5);
+
+    // Results screen
+    setGameStage(stages[2].name);
+  };
 
   const returnStart = () => {
     setGameStage(stages[0].name);
@@ -39,7 +103,7 @@ function App() {
   return (
     <div className='App'> 
       {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-      {gameStage === 'game' && <GameScreen verifyLetter={verifyLetter}/>}
+      {gameStage === 'game' && <GameScreen verifyLetter={verifyLetter} pickedCategory={pickedCategory} pickedWord={pickedWord} letters={letters} guessedLetters={guessedLetters} wrongLetters={wrongLetters} retries={retries} score={score}/>}
       {gameStage === 'results' && <ResultsScreen returnStart={returnStart}/>}
     </div>
   )
--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,10 +1,47 @@
+import { useRef, useState } from 'react';
 import './GameScreen.css'
 
-const GameScreen = ({verifyLetter}) => {
+const GameScreen = ({verifyLetter, pickedCategory, pickedWord, letters, guessedLetters, wrongLetters, retries, score}) => {
+
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+    letterInputRef.current.focus();
+  }
+
   return (
-    <div>
+    <div className='gameScreen'>
       <h1>Secret Word</h1>
-      <button onClick={verifyLetter}>Finalizar o jogo</button>
+      <p className='points'>Pontuação: {score}</p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className='tip'>Dica: <span>{pickedCategory}</span></h3>
+      <p>Você ainda tem {retries} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter) ? ( 
+            <span key={i} className='letterContainerFilled'>{letter}</span>
+          ) : (
+            <span key={i} className='letterContainerEmpty'></span>
+          )
+        ))}
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra: </p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name='letter' maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter} ref={letterInputRef}/>
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <span>Letras já utilizadas:</span>
+        <p>{wrongLetters.join(", ")}</p>
+      </div>
     </div>
   )
 }
